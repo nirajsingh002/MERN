67ab25a3c6dbc03eaf8d0f47e8c7fdec8a4053df
nirajsingh002 <nirajsingh002@gmail.com>
2018-12-13 00:44:56 +0530

    dev connector master

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongodbURI:
-    "mongodb://nirajsingh002:nirajsingh$02@ds113443.mlab.com:13443/developersconnector"
+    "mongodb://nirajsingh002:nirajsingh$02@ds113443.mlab.com:13443/developersconnector",
+  secretOrKey: "secret"
 };
