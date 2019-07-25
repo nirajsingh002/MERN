ff81a480d10034b61fa7db7c78f3bd0099d4970a
nirajsingh002 <nirajsingh002@gmail.com>
2019-07-25 23:30:07 +0530

    change in store for redux dev tool

--- client/src/store.js
+++ client/src/store.js
@@ -5,12 +5,15 @@ import rootReducer from "./reducers";
 const intialState = {};
 
 const middleware = [thunk];
+
 const store = createStore(
   rootReducer,
   intialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      : compose
   )
 );
 
